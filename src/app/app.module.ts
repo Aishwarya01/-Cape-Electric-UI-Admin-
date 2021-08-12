aab03575f6247f5d00ee81dfc04cb78aa9dff1ea
CapeSathish <77102126+CapeSathish@users.noreply.github.com>
2021-08-12 19:38:07 +0530

    Removed admin register component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -17,10 +17,9 @@ import { UserUpdateComponent } from './user-update/user-update.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminHomeComponent } from './admin-home/admin-home.component';
-import { AdminRegisterComponent } from './admin-register/admin-register.component';
 import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
 import { MaterialModule } from './material/material.module';
-import { MsalModule, MsalService, MSAL_INSTANCE, MSAL_INTERCEPTOR_CONFIG } from '@azure/msal-angular';
+import { MsalModule, MsalService, MSAL_INSTANCE } from '@azure/msal-angular';
 import { IPublicClientApplication, PublicClientApplication } from '@azure/msal-browser';
 import { AdminLoginPortalComponent } from './admin-login-portal/admin-login-portal.component';
 import { environment } from 'src/environments/environment';
@@ -31,7 +30,6 @@ export function MSALInstanceFactory(): IPublicClientApplication{
       clientId: environment.clientId,
       redirectUri: environment.redirectUri,
       authority: environment.authority
-
     }
   })
 }
@@ -40,7 +38,6 @@ export function MSALInstanceFactory(): IPublicClientApplication{
 @NgModule({
   declarations: [
     AppComponent,
-    AdminRegisterComponent,
     AdminHomeComponent,
     UserUpdateComponent,
     AdminLoginPortalComponent,
